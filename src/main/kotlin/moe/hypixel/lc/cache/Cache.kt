5225b9495bd0de776d9f83a032146e43d9ba71e8
Alex Howe <mail@ahowe.dev>
2021-12-13 02:24:35 +0000

    lunar was not happy

--- src/main/kotlin/moe/hypixel/lc/cache/Cache.kt
+++ src/main/kotlin/moe/hypixel/lc/cache/Cache.kt
@@ -10,24 +10,4 @@ import moe.hypixel.lc.utils.asString
 import java.util.*
 
 class Cache(val redis: AsyncRedis) {
-	suspend fun playerExists(id: UUID): Boolean {
-		val idStr = id.asString()
-
-		val redisData = redis.get("redis-player-cache:${idStr}")
-		if(redisData != null) {
-			return redisData == "true"
-		} else {
-			val client = HttpClient(CIO)
-			val res = client.request<HttpResponse>("https://sessionserver.mojang.com/session/minecraft/profile/${idStr}")
-			if(res.status == HttpStatusCode.OK) {
-				redis.set("redis-player-cache:${idStr}", "true")
-				return true
-			} else if(res.status == HttpStatusCode.NoContent) {
-				redis.set("redis-player-cache:${idStr}", "false")
-				return false
-			}
-
-			return false
-		}
-	}
 }
